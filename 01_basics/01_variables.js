53fdf6e8d7d26135ef64ec38ab4b9f422ffe2f3a
gitswapnil1 <105999539+gitswapnil1@users.noreply.github.com>
2024-05-20 08:37:32 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,18 @@
+const accountId = 144552
+let accountEmail = "ssd@google.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+let accountState;
+
+
+// accountId = 144562 not allowed
+
+accountEmail = "gc@yahoo.com"
+accountPassword = "908as"
+accountCity="Jaipur"
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
 
