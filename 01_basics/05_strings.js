9766202ba8f7ae49704e08c520205f35b7d39c02
gitswapnil1 <105999539+gitswapnil1@users.noreply.github.com>
2024-05-26 10:47:57 +0000

    strings_covered

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -26,12 +26,36 @@ const anotherString = gameName.slice(-10, 5);
 // console.log(anotherString);
 
 const newStringOne= "  swapnil   "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "http:/swapil.com/swapnil%20form"
-console.log(url.replace('%20', '-'));
+const url = "http:/Swapil.com/swapnil%20form"
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('swapnil'));
+// console.log(gameName.split('-'))
 
-console.log(url.includes('swapnil'));
+// console.log(url.replaceAll('/', '.'));
+console.log(url.toLocaleLowerCase());
+console.log(url.toLowerCase());
+console.log(url.toUpperCase());
 
-console.log(gameName.split('-'))
+const str = 'The quick brown fox jumps over the lazy dog and dog!!';
+const words = str.split(" ");
+const chars = str.split("");
+console.log(chars[4]);
+
+console.log(str.charAt(1));
+console.log(str.concat('and cat'));
+console.log(str.endsWith('dog.'));
+let searchTerm = "dog"
+console.log(str.lastIndexOf(searchTerm));
+
+const paragraph = 'The quick A brown fox jumps over the lazy dog. It barked.';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
+console.log(found);
+
+// console.log(paragraph.repeat(2));
+
+console.log(paragraph.slice(9, 17));
+
